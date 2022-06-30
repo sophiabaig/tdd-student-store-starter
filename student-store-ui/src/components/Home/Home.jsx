7eb1bb9f52523d8bb6a3e7ba770e4a73f9ee99aa
Anthony Nguyen <an@clear.sh>
2022-06-30 13:47:25 -0700

    [small] Map over category list
    Always good to try to reduce copy-paste whenever possible. Doing it
    like this will make it easier to make changes to all buttons without
    having to do it 5 times (or however many times as categories you have).

--- student-store-ui/src/components/Home/Home.jsx
+++ student-store-ui/src/components/Home/Home.jsx
@@ -4,16 +4,20 @@ import "./Home.css"
 import Hero from "../Hero/Hero"
 import ProductGrid from "../ProductGrid/ProductGrid"
 
+const categories = [
+  {id: "all", name: "All Categories"},
+  {id: "clothing", name: "Clothing"},
+  {id: "food", name: "Food"},
+  {id: "accessories", name: "Accessories"},
+  {id: "tech", name: "Tech"},
+];
+
 export default function Home(props) {
   return (
     <div className="home">
       <Hero />
       <input type="text" className="search-bar" placeholder="Search..." name="search" value={props.search} onChange={props.handleOnSearchChange}/>
-      <button className="category" onClick={() => props.handleCategoryChange("all")}>All Categories</button>
-      <button className="category" onClick={() => props.handleCategoryChange("clothing")}>Clothing</button>
-      <button className="category" onClick={() => props.handleCategoryChange("food")}>Food</button>
-      <button className="category" onClick={() => props.handleCategoryChange("accessories")}>Accessories</button>
-      <button className="category" onClick={() => props.handleCategoryChange("tech")}>Tech</button>
+      {categories.map(category => <button className="category" onClick={() => props.handleCategoryChange(category.id)}>{category.name}</button>)};
       <ProductGrid products={props.products} shoppingCart={props.shoppingCart} handleAddItemToCart={props.handleAddItemToCart} handleRemoveItemFromCart={props.handleRemoveItemFromCart}/>
     </div>
   )
